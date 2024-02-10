2944d86851fb18fd3afb1b82dbf1acd45d085860
Tommy Boi <tomaarongrabowski@gmail.com>
2024-02-09 21:26:24 -0500

    Updating _Layout.cshtml file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./Views/**/*.cshtml"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        velblue: '#3097b9',
+        velgreen: '#40bf89',
+        velorange: '#f3a64b',
+        velred: '#fd5a4b',
+        velyellow: '#f8dc5a',
+        velyellowgreen: '#d9e252',
+        veldarkgray: '#21292e',
+        velgray: '#263238',
+        vellightgray: '#bbc1c7',
+        vellink: '#56bfb5',
+        officegreen: '#008000',
+        officelightgreen: '#009e00',
+      }
+    },
   },
   plugins: [],
 };
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.getElementById('toggleAside').addEventListener('click', function() {
+    const sidebarToggle = document.getElementById(
+        'sidebar-toggle',
+      );
+      sidebarToggle.addEventListener('change', () => {
+        setSidebarOpen(!sidebarToggle.checked);
+      });
+});
