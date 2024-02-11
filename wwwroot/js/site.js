a73902a6b313f2b396417b2954572b4c7ce2c542
Tammy Wiley <tomaarongrabowski@gmail.com>
2024-02-11 10:54:01 -0500

    Added functionality to automatically open / close sidebar depending on screen width

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const sidebarToggle = document.getElementById('sidebar-toggle');
+const mediaQuery = window.matchMedia('(min-width: 768px)');
+
+function handleSidebarToggle(mediaQuery) {
+  if (mediaQuery.matches) {
+    sidebarToggle.checked = true;
+  } else {
+    sidebarToggle.checked = false; 
+  }
+}
+
+// Initial check on page load 
+handleSidebarToggle(mediaQuery);
+
+// Listen for screen resize
+mediaQuery.addEventListener('change', handleSidebarToggle); 
