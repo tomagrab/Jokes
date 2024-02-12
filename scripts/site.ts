8bd56adaf42b49da01f697ceb0b957ed2c3e9e06
Tommy Grabowski <60988025+tomagrab@users.noreply.github.com>
2024-02-11 19:27:05 -0500

    Implemented TypeScript

--- scripts/site.ts
+++ scripts/site.ts
@@ -1,18 +1,20 @@
 // Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
+
 // Write your JavaScript code.
-var sidebarToggle = document.getElementById('sidebar-toggle');
-var mediaQuery = window.matchMedia('(min-width: 768px)');
+const sidebarToggle = document.getElementById('sidebar-toggle') as HTMLInputElement;
+const mediaQuery = window.matchMedia('(min-width: 768px)');
+
 function handleSidebarToggle(mediaQuery) {
-    if (mediaQuery.matches) {
-        sidebarToggle.checked = true;
-    }
-    else {
-        sidebarToggle.checked = false;
-    }
+  if (mediaQuery.matches) {
+    sidebarToggle.checked = true;
+  } else {
+    sidebarToggle.checked = false; 
+  }
 }
+
 // Initial check on page load 
 handleSidebarToggle(mediaQuery);
+
 // Listen for screen resize
-mediaQuery.addEventListener('change', handleSidebarToggle);
-//# sourceMappingURL=site.js.map
+mediaQuery.addEventListener('change', handleSidebarToggle); 
